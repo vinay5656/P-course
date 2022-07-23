7a73618d2c6936c03fcfab513c768f6b1e675526
Vinay Singh Chouhan <78366114+vinay5656@users.noreply.github.com>
2022-07-23 11:19:21 +0530

    Add files via upload

---
+++ Junit Project/A Video Rental Inventory System/AllTests.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+    { VideoStoreTest.class, VideoTest.class }
+    )
+
+
+    
+public class AllTests {
+
+}
